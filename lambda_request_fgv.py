5077395fb70d165411daa3501d5c5eebceb5a3e1
akio32 <oliver.akio@gmail.com>
2023-04-23 21:42:21 -0300

    lambda handlers

--- lambda_request_fgv.py
+++ lambda_request_fgv.py
@@ -1,7 +1,22 @@
 import requests
+import json
 
 def lambda_api_call(path):
 
-    response = requests.get(path)
+    # Open the Requests Session
+    session = requests.Session()
 
+    # GET request from the opened session
+    try:
+        response = session.get(path, verify=False)
+    except:
+        return False
 
+    # Check if status_code == 200 to return the response
+    if response.status_code == 200:
+        return response.json()
+
+def lambda_orgs():
+
+    url = 'https://sistema-registropublicodeemissoes.fgv.br/public/organizations/'
+    fgv_orgs = lambda_api_call(url)
