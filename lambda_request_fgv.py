d76c0e5de38eae2a69d465d791aa6ce6a99539a1
akio32 <oliver.akio@gmail.com>
2023-04-26 13:36:04 -0300

    ajuste nas funcoes e modelo de tabela de inventarios

--- lambda_request_fgv.py
+++ lambda_request_fgv.py
@@ -10,121 +10,85 @@ def lambda_handler(event, context):
     :param context: Output of the execution
     :return: True if file was uploaded, else False
     """
-
-    url = 'https://sistema-registropublicodeemissoes.fgv.br/public/organizations/'
-
     response = fgv_orgs(event)
 
 def fgv_orgs(event):
 
-    if event['orgs'] is None:
-
-        fgv_orgs = libs.lambda_api_call(url)
-
-        for org_id in fgv_orgs:
-            org_url = url + str(org_id['_id'])
-            org_detail = libs.lambda_api_call(org_url)
-
-            # Upload the response
-            try:
-                upload_s3 = libs.upload_s3_object(
-                    json_string=json.dumps(org_detail['organization']), 
-                    bucket_name=event['bucket_name'], 
-                    folder_name=event['folder_name'],
-                    object_name=event['object_name'] + str(org_id['_id']) + '.json'
-                )
-
-                return {
-                    'status_code': 200,
-                    'body' : upload_s3
-                }
-            except ClientError as e:
-                return {
-                    'status_code': 400,
-                    'body' : e
-                }
-    else:
-
-        for id in event['orgs']:
-            org_url = url + str(id)
-            org_detail = libs.lambda_api_call(org_url)
-
-            # Upload the response
-            try:
-                upload_s3 = libs.upload_s3_object(
-                    json_string=json.dumps(org_detail['organization']), 
-                    bucket_name=event['bucket_name'], 
-                    folder_name=event['folder_name'], 
-                    object_name=event['object_name'] + str(id).zfill(6) + '.json'
-                )
-
-                return {
-                    'status_code': 200,
-                    'body' : upload_s3
-                }
-            except ClientError as e:
-                return {
-                    'status_code': 400,
-                    'body' : e
-                }
-
-def fgv_inventories(event):
+    url = 'https://sistema-registropublicodeemissoes.fgv.br/'
+    endpoint = '/public/organizations/'
 
     if event['orgs'] is None:
 
-        fgv_orgs = libs.lambda_api_call(url)
+        fgv_orgs = libs.lambda_api_call(url + endpoint)
 
         for org_id in fgv_orgs:
-            org_url = url + str(org_id['_id'])
+            org_url = url + endpoint + str(org_id['_id'])
             org_detail = libs.lambda_api_call(org_url)
 
             # Upload the response
             try:
-                upload_s3 = libs.upload_s3_object(
+                upload_org = libs.upload_s3_object(
                     json_string=json.dumps(org_detail['organization']), 
                     bucket_name=event['bucket_name'], 
-                    folder_name=event['folder_name'],
-                    object_name=event['object_name'] + str(org_id['_id']) + '.json'
+                    folder_name=event['org_folder'],
+                    object_name=event['org_object'] + str(org_id['_id']).zfill(6) + '.json'
                 )
 
-                return {
-                    'status_code': 200,
-                    'body' : upload_s3
-                }
-            except ClientError as e:
+                upload_inv = libs.upload_s3_object(
+                    json_string=json.dumps(org_detail['inventories']), 
+                    bucket_name=event['bucket_name'], 
+                    folder_name=event['inv_folder'],
+                    object_name=event['inv_object'] + str(org_id['_id']).zfill(6) + '.json'
+                )
+            except:
                 return {
                     'status_code': 400,
-                    'body' : e
+                    'body' : 'Erro ao realizar upload do arquivo'
                 }
+        
+        return {
+            'status_code': 200,
+            'body' : 'Upload realizado com sucesso'
+        }
     else:
 
         for id in event['orgs']:
-            org_url = url + str(id)
+            org_url = url + endpoint + str(id)
             org_detail = libs.lambda_api_call(org_url)
 
             # Upload the response
             try:
-                upload_s3 = libs.upload_s3_object(
+                upload_org = libs.upload_s3_object(
                     json_string=json.dumps(org_detail['organization']), 
                     bucket_name=event['bucket_name'], 
-                    folder_name=event['folder_name'], 
-                    object_name=event['object_name'] + str(id).zfill(6) + '.json'
+                    folder_name=event['org_folder'], 
+                    object_name=event['org_object'] + str(id).zfill(6) + '.json'
                 )
 
-                return {
-                    'status_code': 200,
-                    'body' : upload_s3
-                }
-            except ClientError as e:
+                for inv in org_detail['inventories']:
+                    upload_inv = libs.upload_s3_object(
+                        json_string=json.dumps(inv), 
+                        bucket_name=event['bucket_name'], 
+                        folder_name=event['inv_folder'],
+                        object_name=event['inv_object'] + str(id).zfill(6) + '-' + str([inv['_id']]).zfill(6) + '.json'
+                    )
+                
+            except:
                 return {
                     'status_code': 400,
-                    'body' : e
+                    'body' : 'Erro ao realizar upload do arquivo'
                 }
+        return {
+            'status_code': 200,
+            'body' : 'Upload realizado com sucesso'
+        }
 
 payload = {
     "bucket_name" : "uati-case-fgv",
-    "folder_name" : "emissions-orgs",
-    "object_name" : "emissions-orgs-",
+    "org_folder" : "emissions-fgv-org",
+    "org_object" : "emissions-fgv-org-",
+    "inv_folder" : "emissions-fgv-inv",
+    "inv_object" : "emissions-fgv-inv-",
     "orgs" : [1569, 990]
 }
 test = lambda_handler(event=payload, context=None)
@@ -135,4 +99,4 @@ test = lambda_handler(event=payload, context=None)
 ## |C| 1.0. Call the API endpoint "/organizations" to GET the JSON dict with all the companies IDs
 ## |C| 2.0. Call the API endpoint "/organizations/[_id]" to GET the JSON dict with all the companie details
 ## |C| 2.1. Get the JSON response and save in a S3 bucket
-## |C| 2.2. Get the the 
+## |C| 2.1. Append the 
